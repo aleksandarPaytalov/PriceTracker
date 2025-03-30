4e3e6a3c79905743fa6da691bb65e95456e28f24
aleksandarPaytalov <apaytalov89@gmail.com>
2025-03-30 15:44:18 +0300

    Created MonthlyBudged(MB) Db model. Month Enum class added aswell to complete the MB Db model

---
+++ PriceTracker.Infrastructure/Data/Models/MonthlyBudged.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PriceTracker.Infrastructure.Data.Models
+{
+    [Comment("MonthlyBudged Db model")]
+    public class MonthlyBudged
+    {
+        [Key]
+        [Comment("Budged identifier")]
+		public int BudgedId { get; set; }
+
+        [Required]
+        [Comment("User identifier")]
+		public int UserId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(UserId))]
+        [Comment("User navigation property")]
+        public User User { get; set; } = null!;
+
+        [Required]
+        [Comment("Total amount of money or budged we have for the current month")]
+		public decimal BudgedAmount { get; set; }
+
+        [Required]
+        [Comment("Month we spend current budged in")]
+		public Month Month { get; set; } 
+	}
+}
