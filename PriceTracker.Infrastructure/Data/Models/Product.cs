2406327043ca98e53a66ca794e92a64719626d1d
aleksandarPaytalov <apaytalov89@gmail.com>
2025-03-30 15:42:42 +0300

    Created Product Db model

---
+++ PriceTracker.Infrastructure/Data/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PriceTracker.Infrastructure.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace PriceTracker.Infrastructure.Data.Models
+{
+	[Comment("Product Db model")]
+    public class Product
+    {
+		[Key]
+		[Comment("Product identifier")]
+		public int ProductId { get; set; }
+
+		[Required]
+		[Comment("Product quantity")]
+		public int Quantity { get; set; }
+
+		[Required]
+		[StringLength(DataConstants.productNameMaxLength)]
+		[Comment("Product name")]
+		public string ProductName { get; set; } = string.Empty;
+
+		[Required]
+		[StringLength(DataConstants.productBrandNameMaxLength)]
+		[Comment("Product brand")]
+		public string Brand { get; set; } = string.Empty;
+
+		[Required]
+		[StringLength(DataConstants.productCategoryMaxLength)]
+		[Comment("Product category")]
+		public ProductCategory Category { get; set; }
+	}
+}
