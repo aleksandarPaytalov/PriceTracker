a3975d64613ec2db5bc861f2f442d7321803f956
aleksandarPaytalov <apaytalov89@gmail.com>
2025-03-31 23:37:36 +0300

    TaskPriority and TaskStatus Db models have been created

---
+++ PriceTracker.Infrastructure/Data/Models/TaskPriority.cs
@@ -0,0 +1,9 @@
+﻿namespace PriceTracker.Infrastructure.Data.Models
+{
+    public enum TaskPriority
+    {
+        Low = 1, 
+        Medium = 2,
+        High = 3
+    }
+}
---
+++ PriceTracker.Infrastructure/Data/Models/TaskStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace PriceTracker.Infrastructure.Data.Models
+{
+    public enum TaskStatus
+    {
+        Pending = 1,
+        InProgress = 2, 
+        Completed = 3
+    }
+}
