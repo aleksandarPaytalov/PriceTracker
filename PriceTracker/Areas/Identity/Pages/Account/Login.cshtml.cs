64061ffb1c43759c892294ea6642ac7cb85c1c1d
aleksandarPaytalov <apaytalov89@gmail.com>
2025-05-05 22:56:54 +0300

    IdentityUser replaced with the curred class User in order for all the authentication and authorization to work properly

--- PriceTracker/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ PriceTracker/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -2,27 +2,21 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+using PriceTracker.Infrastructure.Data.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace PriceTracker.Areas.Identity.Pages.Account
 {
     public class LoginModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly ILogger<LoginModel> _logger;
+        private readonly SignInManager<User> _signInManager;
+        private readonly ILogger<User> _logger;
 
-        public LoginModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger)
+        public LoginModel(SignInManager<User> signInManager, ILogger<User> logger)
         {
             _signInManager = signInManager;
             _logger = logger;
