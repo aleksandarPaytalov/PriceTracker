82f084397bcde6b39a971f85a0693aea40860693
aleksandarPaytalov <apaytalov89@gmail.com>
2025-05-08 12:30:19 +0300

    MonthlyBudgetConfiguration class created

---
+++ PriceTracker.Infrastructure/Data/SeedDatabase/Configurations/MonthlyBudgetConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PriceTracker.Infrastructure.Data.Models;
+
+namespace PriceTracker.Infrastructure.Data.SeedDatabase.Configurations
+{
+	public class MonthlyBudgetConfiguration : BaseConfiguration<MonthlyBudget>
+	{
+		public MonthlyBudgetConfiguration(IDataProvider<MonthlyBudget> dataProvider) : base(dataProvider)
+		{
+		}
+
+		protected override void ConfigureEntity(EntityTypeBuilder<MonthlyBudget> builder)
+		{
+			// Unique composite index
+			builder.HasIndex(mb => new { mb.UserId, mb.Month })
+				   .IsUnique();
+
+			// Configuration of the relations
+			// We keep history for tracking the monthly budget
+			builder.HasOne(mb => mb.User)
+				   .WithMany(u => u.MonthlyBudgets)
+				   .HasForeignKey(mb => mb.UserId)
+				   .OnDelete(DeleteBehavior.Restrict);
+		}
+	}
+}
