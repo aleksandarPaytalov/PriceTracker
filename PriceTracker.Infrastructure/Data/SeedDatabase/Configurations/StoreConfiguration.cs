e0bc0d041e701a88d4012d603c8b73fbb37b4edb
aleksandarPaytalov <apaytalov89@gmail.com>
2025-05-08 12:31:12 +0300

    StoreConfiguration class created

---
+++ PriceTracker.Infrastructure/Data/SeedDatabase/Configurations/StoreConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PriceTracker.Infrastructure.Data.Models;
+
+namespace PriceTracker.Infrastructure.Data.SeedDatabase.Configurations
+{
+	public class StoreConfiguration : BaseConfiguration<Store>
+	{
+		public StoreConfiguration(IDataProvider<Store> dataProvider) : base(dataProvider)
+		{
+		}
+
+		protected override void ConfigureEntity(EntityTypeBuilder<Store> builder)
+		{
+			// Unique index for the Store name
+			builder.HasIndex(s => s.Name)
+				   .IsUnique();
+
+			// Configuration of the relations
+			builder.HasMany(s => s.Prices)
+				   .WithOne(p => p.Store)
+				   .HasForeignKey(p => p.StoreId)
+				   .OnDelete(DeleteBehavior.Restrict);  
+
+			builder.HasMany(s => s.Expenses)
+				   .WithOne(e => e.Store)
+				   .HasForeignKey(e => e.StoreId)
+				   .OnDelete(DeleteBehavior.Restrict);  
+		}
+	}
+}
