bd9f8e2a9de7cd72565d136a494759ed062a7b74
aleksandarPaytalov <apaytalov89@gmail.com>
2025-05-08 12:30:56 +0300

    ProductConfiguration class created

---
+++ PriceTracker.Infrastructure/Data/SeedDatabase/Configurations/ProductConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PriceTracker.Infrastructure.Data.Models;
+
+namespace PriceTracker.Infrastructure.Data.SeedDatabase.Configurations
+{
+	public class ProductConfiguration : BaseConfiguration<Product>
+	{
+		public ProductConfiguration(IDataProvider<Product> dataProvider) : base(dataProvider)
+		{
+		}
+
+		protected override void ConfigureEntity(EntityTypeBuilder<Product> builder)
+		{
+			// Configuration of the relations
+			builder.HasMany(p => p.Prices)
+				   .WithOne(pr => pr.Product)
+				   .HasForeignKey(pr => pr.ProductId)
+				   .OnDelete(DeleteBehavior.Restrict);  
+
+			builder.HasMany(p => p.Expenses)
+				   .WithOne(e => e.Product)
+				   .HasForeignKey(e => e.ProductId)
+				   .OnDelete(DeleteBehavior.Restrict);
+		}
+	}
+}
