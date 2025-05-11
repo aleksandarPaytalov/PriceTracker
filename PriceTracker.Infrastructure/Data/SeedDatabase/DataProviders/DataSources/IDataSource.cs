bde30a3760fe1f81d9441ca40acc6d5df4be54d5
aleksandarPaytalov <apaytalov89@gmail.com>
2025-05-11 15:46:03 +0300

    IDataSource and JsonDataSource moved in separate folder for better project structure

---
+++ PriceTracker.Infrastructure/Data/SeedDatabase/DataProviders/DataSources/IDataSource.cs
@@ -0,0 +1,7 @@
+﻿namespace PriceTracker.Infrastructure.Data.SeedDatabase.DataProviders.DataSources
+{
+	public interface IDataSource<T> where T : class
+	{
+		Task<IEnumerable<T>> LoadDataAsync();
+	}
+}
