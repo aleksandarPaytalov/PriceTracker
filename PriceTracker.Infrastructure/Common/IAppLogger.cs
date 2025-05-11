f8fabd124719c68591a7b433827cb502aa7e71fe
aleksandarPaytalov <apaytalov89@gmail.com>
2025-05-11 15:40:05 +0300

    Renaming the interface name for easier registration in DI container

---
+++ PriceTracker.Infrastructure/Common/IAppLogger.cs
@@ -0,0 +1,9 @@
+﻿namespace PriceTracker.Infrastructure.Common
+{
+	public interface IAppLogger
+	{
+		void LogError(string message, Exception? ex = null);
+		void LogWarning(string message);
+		void LogInformation(string message);
+	}
+}
