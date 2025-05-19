58d8650c5453bfa52c93d766399435577c608b9b
aleksandarPaytalov <apaytalov89@gmail.com>
2025-05-19 09:38:01 +0300

    UserRoles class created for configure the user roles in the app and prep them for seeding in database

---
+++ PriceTracker.Infrastructure/Data/SeedDatabase/Configurations/IdentityConfiguration/UserRoleConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace PriceTracker.Infrastructure.Data.SeedDatabase.Configurations.IdentityConfiguration
+{
+	public class UserRoleConfiguration : IEntityTypeConfiguration<IdentityUserRole<string>>
+	{
+		public void Configure(EntityTypeBuilder<IdentityUserRole<string>> builder)
+		{
+			var data = new SeedData();
+
+			builder.HasData(
+				data.AdminUserRole,
+				data.RegularUserRole,
+				data.GuestUserRole
+			);
+		}
+	}
+}
