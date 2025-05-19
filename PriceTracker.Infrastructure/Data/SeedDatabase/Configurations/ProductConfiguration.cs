2864dc5ec0cdf3edd6e536b8bab6d5e572946358
aleksandarPaytalov <apaytalov89@gmail.com>
2025-05-19 09:32:41 +0300

    class changed to implement logic for seeding/updating the database via migrations

--- PriceTracker.Infrastructure/Data/SeedDatabase/Configurations/ProductConfiguration.cs
+++ PriceTracker.Infrastructure/Data/SeedDatabase/Configurations/ProductConfiguration.cs
@@ -1,28 +1,38 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using PriceTracker.Infrastructure.Data.Models;
-using PriceTracker.Infrastructure.Data.SeedDatabase.DataProviders;
 
 namespace PriceTracker.Infrastructure.Data.SeedDatabase.Configurations
 {
-	public class ProductConfiguration : BaseConfiguration<Product>
+	public class ProductConfiguration : IEntityTypeConfiguration<Product>
 	{
-		public ProductConfiguration(IDataProvider<Product> dataProvider) : base(dataProvider)
-		{
-		}
-
-		protected override void ConfigureEntity(EntityTypeBuilder<Product> builder)
+		public void Configure(EntityTypeBuilder<Product> builder)
 		{
 			// Configuration of the relations
 			builder.HasMany(p => p.Prices)
 				   .WithOne(pr => pr.Product)
 				   .HasForeignKey(pr => pr.ProductId)
-				   .OnDelete(DeleteBehavior.Restrict);  
+				   .OnDelete(DeleteBehavior.Restrict);
 
 			builder.HasMany(p => p.Expenses)
 				   .WithOne(e => e.Product)
 				   .HasForeignKey(e => e.ProductId)
 				   .OnDelete(DeleteBehavior.Restrict);
+
+			var data = new SeedData();
+
+			builder.HasData(
+				[
+					data.Product1,
+					data.Product2,
+					data.Product3,
+					data.Product4,
+					data.Product5,
+					data.Product6,
+					data.Product7,
+					data.Product8,
+					data.Product9,
+				]);
 		}
 	}
 }
