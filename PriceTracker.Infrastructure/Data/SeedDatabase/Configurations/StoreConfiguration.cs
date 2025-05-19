fa132aeb51aab4bd84040ab14b70be2407e8c573
aleksandarPaytalov <apaytalov89@gmail.com>
2025-05-19 09:32:46 +0300

    class changed to implement logic for seeding/updating the database via migrations

--- PriceTracker.Infrastructure/Data/SeedDatabase/Configurations/StoreConfiguration.cs
+++ PriceTracker.Infrastructure/Data/SeedDatabase/Configurations/StoreConfiguration.cs
@@ -1,32 +1,41 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using PriceTracker.Infrastructure.Data.Models;
-using PriceTracker.Infrastructure.Data.SeedDatabase.DataProviders;
 
 namespace PriceTracker.Infrastructure.Data.SeedDatabase.Configurations
 {
-	public class StoreConfiguration : BaseConfiguration<Store>
+	public class StoreConfiguration : IEntityTypeConfiguration<Store>
 	{
-		public StoreConfiguration(IDataProvider<Store> dataProvider) : base(dataProvider)
-		{
-		}
-
-		protected override void ConfigureEntity(EntityTypeBuilder<Store> builder)
+		public void Configure(EntityTypeBuilder<Store> builder)
 		{
 			// Unique index for the Store name
-			builder.HasIndex(s => s.Name)
-				   .IsUnique();
+			builder.HasIndex(s => s.Name).IsUnique();
 
 			// Configuration of the relations
 			builder.HasMany(s => s.Prices)
 				   .WithOne(p => p.Store)
 				   .HasForeignKey(p => p.StoreId)
-				   .OnDelete(DeleteBehavior.Restrict);  
+				   .OnDelete(DeleteBehavior.Restrict);
 
 			builder.HasMany(s => s.Expenses)
 				   .WithOne(e => e.Store)
 				   .HasForeignKey(e => e.StoreId)
-				   .OnDelete(DeleteBehavior.Restrict);  
+				   .OnDelete(DeleteBehavior.Restrict);
+
+			var data = new SeedData();
+
+			builder.HasData(
+			[
+				data.Store1,
+				data.Store2,
+				data.Store3,
+				data.Store4,
+				data.Store5,
+				data.Store6,
+				data.Store7,
+				data.Store8,
+			]);
+		
 		}
 	}
 }
