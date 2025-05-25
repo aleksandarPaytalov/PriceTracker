6e44736b47293d6ebff1fa2568da69449b530c31
aleksandarPaytalov <apaytalov89@gmail.com>
2025-05-25 09:19:54 +0300

    DataSource and ProductDataProvider Factory classes created to support seeding from external source and better unit testing latter on.

---
+++ PriceTracker.Infrastructure/Data/SeedDatabase/DataProviders/DataSources/DataSourceFactory.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace PriceTracker.Infrastructure.Data.SeedDatabase.DataProviders.DataSources
+{
+	public class DataSourceFactory : IDataSourceFactory
+	{
+		public IDataSource<T> CreateJsonDataSource<T>(string filePath) where T : class
+		{
+			return new JsonDataSource<T>(filePath);
+		}
+	}
+}
---
+++ PriceTracker.Infrastructure/Data/SeedDatabase/DataProviders/DataSources/IDataSourceFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace PriceTracker.Infrastructure.Data.SeedDatabase.DataProviders.DataSources
+{
+	public interface IDataSourceFactory
+	{
+		IDataSource<T> CreateJsonDataSource<T>(string filePath) where T : class;
+	}
+}
