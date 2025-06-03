f333828ff0f2dc95f41a1b99899962eda6f1eb0f
aleksandarPaytalov <apaytalov89@gmail.com>
2025-06-03 11:04:15 +0300

    Code refactoring relevant to email confirmation, passwordreset and welcome email -> better maintrabillity

---
+++ PriceTracker.Core/Services/IEmailTemplateService.cs
@@ -0,0 +1,9 @@
+﻿namespace PriceTracker.Core.Services
+{
+	public interface IEmailTemplateService
+	{
+		Task<string> RenderEmailConfirmationAsync(string callbackUrl);
+		Task<string> RenderPasswordResetAsync(string callbackUrl);
+		Task<string> RenderWelcomeAsync(string userName);
+	}
+}
---
+++ PriceTracker/Models/WelcomeViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace PriceTracker.Models
+{
+	public class WelcomeViewModel
+	{
+		public string UserName { get; set; } = string.Empty;
+	}
+}
